6cc4d4d5db76d0aa944539341a956d254b3216f4
Arthur Martinelli <ama18@inf.ufpr.br>
2022-06-17 00:46:13 -0300

    adicionado modo demo

--- ia/main.py
+++ ia/main.py
@@ -18,6 +18,12 @@ if __name__ == '__main__':
         except:
             print("ERROR: unable to find initial epsilon USING DEFAULT VALUE 1")
 
+    exp_min = 0.05
+    if "-d" in sys.argv or "--demo" in sys.argv:
+        init_exp = 0
+        exp_min = 0
+        use_screen = True
+
     run_agent = \
         run_agent.AgentRun(
             max_episodes = 10000,
@@ -25,7 +31,7 @@ if __name__ == '__main__':
             nn_layers = [128, 64],
             lr = 0.001,
             init_exp = init_exp,
-            exp_min = 0.1,
+            exp_min = exp_min,
             exp_decay = 0.999,
             gamma = 0.99,
             batch_size = 128,
