3b16b84622358ca5010021390a40dbd29c27cdf6
ARTHUR MARTINELLI ANTONIETTO <ama18@abbey.c3sl.ufpr.br>
2023-01-31 00:44:25 -0300

    consertado problema ao salvar rede

--- run_single_test.sh
+++ run_single_test.sh
@@ -11,4 +11,6 @@
 
 module load libraries/cuda/11.8
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/cluster/libraries/cuda/11.8
+export HDF5_USE_FILE_LOCKING='FALSE'
+
 pipenv run python ~/tetris-ia/ia/main.py $@
